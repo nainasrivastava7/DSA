089dee2c686caa528bdc95bc2630f536d3fc3335
Naina Srivastava <nainasrivastava777@gmail.com>
2026-06-26 23:00:53 +0530

    Create ValidParenthesis.cpp

---
+++ Stack & Queue/ValidParenthesis.cpp
@@ -0,0 +1,28 @@
+// My leetcode solution for problem 20
+
+class Solution {
+public:
+    bool isValid(string s) {
+         stack<char> st;
+
+        for(char ch : s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if(st.empty())
+                    return false;
+
+                if((ch == ')' && st.top() == '(') ||
+                   (ch == '}' && st.top() == '{') ||
+                   (ch == ']' && st.top() == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+    
+};
